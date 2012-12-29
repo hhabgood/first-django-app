278112a5a7c44fce4d6922416de96441d6ec754d
Henry Habgood <henryhabgood@Henrys-MacBook-Pro.local>
2012-12-28 23:05:26 -0500

    Added fieldsets to separate out poll data more

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,9 @@ from polls.models import Poll
 from django.contrib import admin
 
 class PollAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question']
+	fieldsets = [
+		(None,					{'fields': ['question']}),
+		('Date Information',	{'fields': ['pub_date']}),
+	]
 
 admin.site.register(Poll, PollAdmin)
