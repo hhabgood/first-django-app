9113cd21bbc52c2eeb92de6c24c8e4ae915a2a64
Henry Habgood <henryhabgood@Henrys-MacBook-Pro.local>
2012-12-28 23:06:34 -0500

    Added collapse class to date data so that it doesn't show up right away

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,					{'fields': ['question']}),
-		('Date Information',	{'fields': ['pub_date']}),
+		('Date Information',	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 
 admin.site.register(Poll, PollAdmin)
